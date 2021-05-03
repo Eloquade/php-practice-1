3219f295b22fadf46e4267c675063268d9eaaf44
Jheave <jimenezjheave123@gmail.com>
2021-05-03 12:15:23 +0800

    fix error

--- practice.php
+++ practice.php
@@ -1,34 +1,41 @@
 <html>
-<body>
-<form action='' method ='POST'>
-    <p>Enter loan:
-        <input type = 'text' name ='principal'/>
-    </p>
-    <p>Enter rate:
-        <input type='text' name='interest' />
-    </p>
-    <p>Enter payment:
-        <input type='text' name='payment'/>
-    </p>
-    <input type='submit' value='submit'/>
-    <input type='reset' value='clear'/>
-    <?php
-            if(isset($_POST['submit']));
-            {
+    <head>
+        <title>Exam</title>
+    </head>
+    <body>
+        <form action='' method ='POST'>
+            <p>Enter loan:
+                <input type = 'text' name ='principal'/>
+            </p>
+            <p>Enter rate:
+                <input type='text' name='interest' />
+            </p>
+            <p>Enter payment:
+                <input type='text' name='payment'/>
+            </p>
+            <input type='submit' value='submit'/>
+            <input type='reset' value='clear'/>
+            <br>
+            <br>
+           <?php
+               if(isset($_POST['submit']));{
+                   $p = $_POST['principal'];
+                   $i = $_POST['interest'];
+                   $pay = $_POST['payment'];
+                   $i = $p * $i;
+                   $a = ($p + $i) - $pay;
 
-                $p= $_POST['principal'];
-                $i= $_POST['interest'];
-                $pay= $_POST['payment'];
-                $i = $p * $i;
-                $p= ($p + $i) - $pay;
-                for($year=1;$year <=7;$year++);
-            {
-                echo "year ". '\t' . "principal" . '\t' . "interest" . '\t' . "payment" .'\n';
-                echo "$year". '\t' . "$p". '\t' . "$i". '\t' . "$pay";
-            }
-            }
-    ?>
+                   printf($a);
+                   // for($year=1;$year <=7;$year++)
+                   // {
+                   //     echo "year ". '\t' . "principal" . '\t' . "interest" . '\t' . "payment" .'\n';
+                   //     echo "$year". '\t' . "$p". '\t' . "$i". '\t' . "$pay";
+                   // }
+                   
+               }
+          ?>
 
-</form>
-</body>
+        </form>
+        
+    </body>
 </html>
