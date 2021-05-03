a0766b547faeb04234a5dbee76766423de8aa8b1
Eloquade <perez.lawrence08@gmail.com>
2021-05-03 11:38:25 +0800

    practice

---
+++ practice.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+<form action='' method ='POST'>
+    <p>Enter loan:
+        <input type = 'text' name ='principal'/>
+    </p>
+    <p>Enter rate:
+        <input type='text' name='interest' />
+    </p>
+    <p>Enter payment:
+        <input type='text' name='payment'/>
+    </p>
+    <input type='submit' value='submit'/>
+    <input type='reset' value='clear'/>
+    <?php
+            if(isset($_POST['submit']));
+            {
+
+                $p= $_POST['principal'];
+                $i= $_POST['interest'];
+                $pay= $_POST['payment'];
+                $i = $p * $i;
+                $p= ($p + $i) - $pay;
+                for($year=1;$year <=7;$year++);
+            {
+                echo "year ". '\t' . "principal" . '\t' . "interest" . '\t' . "payment" .'\n';
+                echo "$year". '\t' . "$p". '\t' . "$i". '\t' . "$pay";
+            }
+            }
+    ?>
+
+</form>
+</body>
+</html>
